be5132eec2e237ac93fa64f29c6aa39ecb98a28c
Eduardo Rodrigues <contatoeduardofelipe@hotmail.com>
2024-05-07 19:23:28 -0300

    closes #3 - add a form to register a car

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormComponent } from "./components/form/form.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, FormComponent]
 })
 export class AppComponent {
   title = 'bootcamp-fatec-projeto08';

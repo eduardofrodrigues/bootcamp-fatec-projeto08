73e2d5a235515f9637dfa3c8aba4a1afbf3acca3
Eduardo Rodrigues <contatoeduardofelipe@hotmail.com>
2024-05-07 19:56:20 -0300

    closes #4 - add a table to registered cars

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormComponent } from "./components/form/form.component";
+import { TableComponent } from "./components/table/table.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, FormComponent]
+  imports: [RouterOutlet, FormComponent, TableComponent]
 })
 export class AppComponent {
   title = 'bootcamp-fatec-projeto08';

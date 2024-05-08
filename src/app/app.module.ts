429a26b53940fcf757a1eea2f71b2d0e0bb1cad3
Eduardo Rodrigues <contatoeduardofelipe@hotmail.com>
2024-05-07 23:01:13 -0300

    #6 - add a header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { FormComponent } from "./components/form/form.component";
 import { TableComponent } from "./components/table/table.component";
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FormComponent,
     TableComponent,
+    HeaderComponent,
   ],
   providers: [],
   bootstrap: [AppComponent],
